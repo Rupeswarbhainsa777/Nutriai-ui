40c2ed204558eb873555e1c6f99c1419b0a183a5
Rupeswar Bhainsa <rupeswar77666@gmail.com>
2026-04-16 22:48:42 +0530

    all path are set in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,39 @@
 import './App.css'
 import Dashboard from "./pages/dashboard/Dashboard.jsx";
+import {RouterProvider, createBrowserRouter} from "react-router-dom";
+import MealPlanner from "./pages/mealplanner/MealPlanner.jsx";
+import Profile from "./pages/profile/Profile.jsx"; // ✅ fixed folder case
+import AIAssistant from "./pages/aiAssistant/AIAssistant.jsx"; // ✅ fixed naming
+import Recipes from "./pages/recipes/Recipes.jsx";
 
 function App() {
 
+    const router = createBrowserRouter([
+        {
+            path: "/",
+            element: <Dashboard/>
+        },
+        {
+            path: "/meal-planner",
+            element: <MealPlanner/>
+        },
+        {
+            path: "/profile",
+            element: <Profile/>
+        },
+        {
+            path: "/ai-assistant",
+            element: <AIAssistant/>
+        },
+        {
+            path: "/recipes",
+            element: <Recipes/>
+        }
+    ]);
 
     return (
-
-         <Dashboard />
-
+        <RouterProvider router={router}/>
     )
 }
 
-export default App
+export default App;
