d1fec2787875970125cdf4f865481637e28aff23
Rupeswar Bhainsa <rupeswar77666@gmail.com>
2026-05-15 02:32:28 +0530

    Reg api hendel in auth.js file

---
+++ src/service/Auth.js
@@ -0,0 +1,18 @@
+const BASE_URL = "http://localhost:1003/api/auth";
+
+export const registration = async (credentials) => {
+    const response = await fetch(`${BASE_URL}/auth/reg`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(credentials),
+
+    })
+
+    const data = await response.json();
+    if (!response.ok) {
+        throw new Error(data.message || "Registration failed.");
+    }
+    return data;
+}
