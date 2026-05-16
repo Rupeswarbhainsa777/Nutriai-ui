8c79b3916d6e9263c14001a3b6db44bc3a4740e7
Rupeswar Bhainsa <rupeswar77666@gmail.com>
2026-05-16 23:06:44 +0530

    all configuration completed for login and handel apis also

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,5 +1,41 @@
+import {useState} from "react";
+import {Link, useNavigate} from "react-router-dom";
+import {loginUser} from "../../service/Auth.js";
 
 const Login = () => {
+    const navigate = useNavigate();
+
+    const [formData, setFormData] = useState({
+        email: "",
+        password: "",
+    });
+
+    const [message, setMessage] = useState("");
+
+    const handleChange = (e) => {
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value,
+        });
+    };
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            const data = await loginUser(formData);
+
+            // Save only user information (JWT removed for now)
+            localStorage.setItem("user", JSON.stringify(data.user));
+
+            setMessage("Login successful!");
+
+            // Redirect to dashboard
+            navigate("/dashboard");
+        } catch (error) {
+            setMessage(error.message);
+        }
+    };
+
     return (
         <h1>Login</h1>
     )
